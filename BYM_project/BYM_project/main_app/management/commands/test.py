6f82947e4518c0b6de7bee6257ed22d59c08eb91
Vladimir <107643964+vvprojects@users.noreply.github.com>
2024-08-16 22:02:57 +0300

    Delete BYM_project/BYM_project directory

--- BYM_project/BYM_project/main_app/management/commands/test.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = 'test'
-
-    def handle(self, *args, **options):
-        pass
--- BYM_project/BYM_project/main_app/recommendation_system.py
+++
@@ -1,9 +0,0 @@
-class RecommendationSystem:
-    def __init__(self):
-        pass
-
-    def fit(self, X, y):
-        return self
-
-    def predict(self, X, y):
-        pass
